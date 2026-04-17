31e5b4782eb80babcec339436ac1aadab27c17b3
likhil <likhilb14@gmail.com>
2026-04-17 14:23:34 +0530

    suii

---
+++ todo/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+import TodoInput from "./components/todo input"
+
+function app(){
+    return(
+        <>
+        <Header/>
+        <TodoInput/>
+        </>
+    )
+}
+export default app
